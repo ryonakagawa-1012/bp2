b47b9ad2f3cf1be2d5ae7f31f3c549fe09318d08
Ryo <ryo.na.na.ryo.na@gmail.com>
2023-07-21 09:19:03 +0900

    first

---
+++ 01/janken2.sh
@@ -0,0 +1,12 @@
+# あいこの場合 
+echo 1 1 | ./janken2
+echo 2 2 | ./janken2
+echo 3 3 | ./janken2
+# 勝ちの場合 
+echo 1 2 | ./janken2
+echo 2 3 | ./janken2
+echo 3 1 | ./janken2
+# 負けの場合 
+echo 1 3 | ./janken2
+echo 2 1 | ./janken2
+echo 3 2 | ./janken2
