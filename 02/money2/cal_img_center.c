5f4b2141f7fc5fe9afb16269161296c3d67ab382
Ryo <ryo.na.na.ryo.na@gmail.com>
2023-07-21 11:32:39 +0900

    update 02

---
+++ 02/money2/cal_img_center.c
@@ -0,0 +1,9 @@
+#include <handy.h>
+#include <stdio.h>
+
+#include <money2.c>
+
+void draw500(int x, int y) {
+    int width, height;
+    int img = HgImageLoad("image/money_coin_reiwa_500.png");
+};
--- 02/money2/money2.c
+++ 02/money2/money2.c
@@ -1,29 +1,33 @@
 #include <handy.h>
 #include <stdio.h>
 
-#define WINDOW_X_SIZE 1000
+#include <cal_img_center.c>
+
+#define WINDOW_X_SIZE 1200
 #define WINDOW_Y_SIZE 600
 
-void money(int);
+#define COIN_Y 100
+
+void money_draw(int, int);  // お金を描画する関数
+
+double x_500_text_size, y_500_text_size;  // 文字(500)の大きさを格納する変数
+double x_100_text_size, y_100_text_size;  // 文字(100)の大きさを格納する変数
+double x_50_text_size, y_50_text_size;  // 文字(50)の大きさを格納する変数
+double x_10_text_size, y_10_text_size;  // 文字(10)の大きさを格納する変数
+double x_5_text_size, y_5_text_size;  // 文字(5)の大きさを格納する変数
+double x_1_text_size, y_1_text_size;  // 文字(1)の大きさを格納する変数
+
+double x_500_text_pos, y_500_text_pos;  // 文字(500)の位置を格納する変数
+double x_100_text_pos, y_100_text_pos;  // 文字(100)の位置を格納する変数
+double x_50_text_pos, y_50_text_pos;  // 文字(50)の位置を格納する変数
+double x_10_text_pos, y_10_text_pos;  // 文字(10)の位置を格納する変数
+double x_5_text_pos, y_5_text_pos;    // 文字(5)の位置を格納する変数
+double x_1_text_pos, y_1_text_pos;    // 文字(1)の位置を格納する変数
 
 int main() {
     int money[6] = {};
     int coin = 500;
     int total = 0;
-
-    double x_500_text_size, y_500_text_size;  // 文字(500)の大きさを格納する変数
-    double x_100_text_size, y_100_text_size;  // 文字(100)の大きさを格納する変数
-    double x_50_text_size, y_50_text_size;  // 文字(50)の大きさを格納する変数
-    double x_10_text_size, y_10_text_size;  // 文字(10)の大きさを格納する変数
-    double x_5_text_size, y_5_text_size;  // 文字(5)の大きさを格納する変数
-    double x_1_text_size, y_1_text_size;  // 文字(1)の大きさを格納する変数
-
-    double x_500_text_pos, y_500_text_pos;  // 文字(500)の位置を格納する変数
-    double x_100_text_pos, y_100_text_pos;  // 文字(100)の位置を格納する変数
-    double x_50_text_pos, y_50_text_pos;  // 文字(50)の位置を格納する変数
-    double x_10_text_pos, y_10_text_pos;  // 文字(10)の位置を格納する変数
-    double x_5_text_pos, y_5_text_pos;  // 文字(5)の位置を格納する変数
-    double x_1_text_pos, y_1_text_pos;  // 文字(1)の位置を格納する変数
 
     int i;  // カウンタ変数
 
@@ -70,17 +74,17 @@ int main() {
         (WINDOW_X_SIZE / 6) * 5 +
         ((WINDOW_X_SIZE / 6) - x_1_text_size) / 2;  // 文字(1)の位置を計算
 
-    HgText(x_500_text_pos, 150, "500");  // 文字(500)を描画
+    HgText(x_500_text_pos, COIN_Y, "500");  // 文字(500)を描画
 
-    HgText(x_100_text_pos, 150, "100");  // 文字(100)を描画
+    HgText(x_100_text_pos, COIN_Y, "100");  // 文字(100)を描画
 
-    HgText(x_50_text_pos, 150, "50");  // 文字(50)を描画
+    HgText(x_50_text_pos, COIN_Y, "50");  // 文字(50)を描画
 
-    HgText(x_10_text_pos, 150, "10");  // 文字(10)を描画
+    HgText(x_10_text_pos, COIN_Y, "10");  // 文字(10)を描画
 
-    HgText(x_5_text_pos, 150, "5");  // 文字(5)を描画
+    HgText(x_5_text_pos, COIN_Y, "5");  // 文字(5)を描画
 
-    HgText(x_1_text_pos, 150, "1");  // 文字(1)を描画
+    HgText(x_1_text_pos, COIN_Y, "1");  // 文字(1)を描画
 
     /**********ここまで**********/
 
@@ -89,6 +93,8 @@ int main() {
         total = total % coin;
 
         printf("%d:%d ", coin, money[i]);
+
+        money_draw(coin, money[i]);
 
         if (i % 2 == 0) {
             coin = coin / 5;
@@ -105,4 +111,30 @@ int main() {
     return 0;
 }
 
-void money(kind) {}
+void money_draw(int kind, int sum) {
+    switch (kind) {
+        case 500:
+
+            break;
+
+        case 100:
+            img = HgImageLoad("image/money_coin_reiwa_100.png");
+            break;
+
+        case 50:
+            img = HgImageLoad("image/money_coin_reiwa_50.png");
+            break;
+
+        case 10:
+            img = HgImageLoad("image/money_coin_reiwa_10.png");
+            break;
+
+        case 5:
+            img = HgImageLoad("image/money_coin_reiwa_5.png");
+            break;
+
+        case 1:
+            img = HgImageLoad("image/money_coin_reiwa_1.png");
+            break;
+    };
+}
