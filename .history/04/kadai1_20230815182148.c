8ec9fee0a3d2fb66348b089c9712c0097b9a96e0
Ryo <ryo.na.na.ryo.na@gmail.com>
2023-08-15 18:27:36 +0900

    Fin kadai1

---
+++ .history/04/kadai1_20230815182148.c
@@ -0,0 +1,24 @@
+#include <handy.h>
+#include <math.h>
+#include <stdio.h>
+
+#define CENTER 200.0
+int main() {
+    double x, y;
+
+    HgOpen(400.0, 400.0);
+
+    HgSetWidth(1.0);
+    HgSetColor(HG_BLACK);
+    HgLine(5.0, 200.0, 395.0, 200.0);
+    HgLine(200.0, 5.0, 200.0, 395.0);
+
+    x = 150.0;
+    y = 90.0;
+    HgSetColor(HG_BLUE);
+    HgCircle(x + 200.0, y + 200.0, 3.0);
+
+    HgGetChar();
+    HgClose();
+    return 0;
+}
