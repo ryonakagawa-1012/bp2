6edd70aad827f0a70964f23f6889513232eddbbc
Ryo <ryo.na.na.ryo.na@gmail.com>
2023-08-15 18:53:56 +0900

    Fin kadai3

---
+++ .history/04/kadai3_20230815184557.c
@@ -0,0 +1,39 @@
+#include <handy.h>
+#include <math.h>
+#include <stdio.h>
+
+#define CENTER 200.0
+int main() {
+    double x, y;
+    double tmp_x, tmp_y;
+    double rad;
+
+    scanf(&rad)
+
+        HgOpen(400.0, 400.0);
+
+    HgSetWidth(1.0);
+    HgSetColor(HG_BLACK);
+    HgLine(5.0, 200.0, 395.0, 200.0);
+    HgLine(200.0, 5.0, 200.0, 395.0);
+
+    x = 150.0;
+    y = 90.0;
+    HgSetColor(HG_BLUE);
+    HgCircle(x + CENTER, y + CENTER, 3.0);
+
+    while () {
+        tmp_x = x;
+        tmp_y = y;
+
+        x = tmp_x * cos(M_PI / 4) - tmp_y * sin(M_PI / 4);
+        y = tmp_x * sin(M_PI / 4) + tmp_y * cos(M_PI / 4);
+
+        HgSetColor(HG_RED);
+        HgCircle(x + CENTER, y + CENTER, 3.0);
+    }
+
+    HgGetChar();
+    HgClose();
+    return 0;
+}
