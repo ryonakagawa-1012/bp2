ba1a2d8a5c2849d754fdea8af7a318c16f8a58a7
Ryo <ryo.na.na.ryo.na@gmail.com>
2023-08-15 10:48:38 +0900

    Fin kadai1

---
+++ .history/03/kadai1_20230815103926.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    int data[5];
+
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &data[i]);
+    }
+
+    for (i = 0; i < 5; i++) {
+        printf("data%d : %d\n", i + 1, data[i]);
+    }
+
+    return 0;
+}
