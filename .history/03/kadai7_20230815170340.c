2580ed5805d81dd5ae8b11e4bb112acaac0d210a
Ryo <ryo.na.na.ryo.na@gmail.com>
2023-08-15 17:17:16 +0900

    Fin kadai7

---
+++ .history/03/kadai7_20230815170340.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define NUM 5
+
+int main() {
+    int i;
+    int max;
+    int data1[NUM], data2[NUM];
+
+    for (i = 0; i < NUM; i++) {
+        scanf("%d", &data1[i]);
+    }
+
+    for (i = 0; i < NUM; i++) {
+        if (i == 0) {
+            max = data[0];
+        } else if (max < data[i]) {
+            max = data[i];
+        }
+    }
+    for (i = 0; i < NUM; i++) {
+        printf("data[%d] : %d\n", i, data2[i]);
+    }
+
+    return 0;
+}
