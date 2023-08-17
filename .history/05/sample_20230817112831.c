debef60ebfccff3128e2af48cfc223bead25fadc
Ryo <ryo.na.na.ryo.na@gmail.com>
2023-08-17 11:30:12 +0900

    Run sample

---
+++ .history/05/sample_20230817112831.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int data[3][5];
+    int i, j, sum;
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 5; j++) {
+            scanf("%d", &data[i][j]);
+        }
+    }
+
+    for (i = 0; i < 3; i++) {
+        sum = 0;
+        for (j = 0; j < 5; j++) {
+            printf(" %4d", data[i][j]);
+            sum += data[i][j];
+        }
+        printf(" : %4d\n", sum);
+    }
+
+    return 0;
+}
