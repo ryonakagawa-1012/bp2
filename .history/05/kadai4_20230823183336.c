b960cd95e905b1748fd01424f2397c8001f9d2d7
Ryo <ryo.na.na.ryo.na@gmail.com>
2023-08-23 18:35:27 +0900

    Fin kadai4

---
+++ .history/05/kadai4_20230823183336.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+#define SIZE 3
+
+int main() {
+    int matrix[SIZE][SIZE] = {};
+    int matrix2[SIZE][SIZE] = {};
+    int x, y;
+    int count = 0;
+
+    for (y = 0; y < SIZE; y++) {
+        for (x = 0; x < SIZE; x++) {
+            scanf("%d", &matrix[x][y]);
+        }
+    }
+
+    /*回転前表示*/
+    printf("step %d\n", count);
+    for (y = 0; y < SIZE; y++) {
+        for (x = 0; x < SIZE; x++) {
+            if (matrix[x][y] == 0)
+                printf("□ ");
+            else
+                printf("■ ");
+        }
+        printf("\n");
+    }
+    /*ここまで*/
+
+    /***ループ***/
+    for (int i = 0; i <= 3; i++) {
+        /*回転操作*/
+        for (y = 0; y < SIZE; y++) {
+            for (x = 0; x < SIZE; x++) {
+                matrix2[SIZE - 1 - x][y] = matrix[y][x];
+            }
+        }
+
+        /*
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                matrix2[j][SIZE - 1 - i] = matrix[i][j];
+            }
+        }
+        */
+        /*ここまで*/
+
+        /*回転後表示*/
+        printf("step %d\n", count);
+        for (y = 0; y < SIZE; y++) {
+            for (x = 0; x < SIZE; x++) {
+                if (matrix2[x][y] == 0)
+                    printf("□ ");
+                else
+                    printf("■ ");
+            }
+            printf("\n");
+        }
+        /*ここまで*/
+
+        /*代入処理*/
+        for (y = 0; y < SIZE; y++) {
+            for (x = 0; x < SIZE; x++) {
+                matrix[x][y] = matrix2[x][y]
+            }
+            printf("\n");
+        }
+        /*ここまで*/
+    }
+    /***ここまで***/
+
+    return 0;
+}
