9132c2dabebf92e0c188e00ba8c20d9fbb062436
Ryo <ryo.na.na.ryo.na@gmail.com>
2023-08-23 18:49:10 +0900

    add sample

---
+++ .history/06/sample_20230823184215.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char string[100];
+    int length;
+    scanf("%s", string);
+    length = 0;
+    while (string[length] != '\0') {
+        length++;
+    }
+    printf("%d\n", length);
+}
