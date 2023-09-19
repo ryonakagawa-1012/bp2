eb10ff2fd9589a0f273ba2acf452d09f7cdf030d
Ryo <ryo.na.na.ryo.na@gmail.com>
2023-09-19 13:29:14 +0900

    Fin lager02

---
+++ .history/01/larger02_20230919131254.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3;
+
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    scanf("%d", &num3);
+
+    if (num1 > num2) {
+        if () printf("answer: %d\n", num1);
+
+        return 0;
+    }
+}
---
+++ .history/01/larger02_20230919131904.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3;
+
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    scanf("%d", &num3);
+
+    if (num1 > num2) {
+        if (num1 > num3) {
+            printf("answer: %d\n", num1);
+        } else {
+            printf("answer: %d\n", num3);
+        }
+    } else {
+        if (num2 > num3) {
+        }
+    }
+
+    return 0;
+}
