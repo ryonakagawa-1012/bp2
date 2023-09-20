2d74027e55bb9e952731acca44e13919a5a719ae
Ryo <ryo.na.na.ryo.na@gmail.com>
2023-09-20 12:48:18 +0900

    [Fix] 説明と名前の追加

--- .history/01/janken2_20230920111725.c
+++ .history/01/janken2_20230920111725.c
@@ -3,6 +3,7 @@
     一つ目は「自分の手」、二つ目は「相手の手」
     1: グー 2: チョキ 3: パー
     勝ち負けは “win”, “lose”, “even” で表示する
+    g2354017 中川 諒
 */
 
 #include <stdio.h>
@@ -12,11 +13,21 @@
 #define PARPER 3
 
 int main() {
-    int my_hand, enemy_hand;
+    int my_hand, enemy_hand, result;
 
     scanf("%d", &my_hand);
 
     scanf("%d", &enemy_hand);
+
+    result = my_hand - enemy_hand;
+
+    if (result == 0) {
+        printf("even\n");
+    } else if (result == -1 || result == 2) {
+        printf("win\n");
+    } else {
+        printf("lose\n");
+    }
 
     return 0;
 }
